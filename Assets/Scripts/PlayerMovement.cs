b24d5f09f2800b84ce1f640e19d7860a0bb74874
Iván Alexander Aráoz Andrade <iaraoz061102@gmail.com>
2025-10-02 18:53:23 -0500

    Add player animation system and assets
    Introduced Unity Animator Controller for the player with idle, walk, jump, fly, and fall states. Added corresponding animation clips and sprites, updated Player prefab to use CapsuleCollider2D and Animator, and moved PlayerMovement script to Scripts folder. Also added a test scene for animations and updated meta files for new and changed assets.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
     [SerializeField] private float speed = 5f;
     
     private float _horizontalMovement;
@@ -16,6 +17,8 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = new Vector2(_horizontalMovement * speed, rb.linearVelocity.y);
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
+        animator.SetFloat("magnitude", rb.linearVelocity.magnitude);
     }
 
     public void Move(InputAction.CallbackContext context)

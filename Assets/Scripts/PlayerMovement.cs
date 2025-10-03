d61ac68c48d8dded0a4a913943e2a09e22a1460e
Iván Alexander Aráoz Andrade <iaraoz061102@gmail.com>
2025-10-02 19:47:15 -0500

    Add jump functionality and improve player movement
    Introduces jump input mapping and event binding, updates Player prefab with ground check and physics material, and enhances PlayerMovement script with jump logic and fall parameters. Updates animation controller and input actions to support jumping and falling states, and adds a test scene for jump mechanics.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,28 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator animator;
-    [SerializeField] private float speed = 5f;
     
+    [Header("Movement")]
+    [SerializeField] private float speed = 5f;
     private float _horizontalMovement;
     
-    void Start()
-    {
-        
-    }
+    [Header("Jumping")]
+    [SerializeField] private float jumpPower = 10f;
+    
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private Vector2 groundCheckSize = new Vector2(0.5f, 0.5f);
+    [SerializeField] private LayerMask groundLayer;
 
+    [Header("Gravity")] 
+    [SerializeField] private float baseGravity = 2f;
+    [SerializeField] private float maxFallSpeed = 18f;
+    [SerializeField] private float fallSpeedMultiplier = 2f;
+    
     void Update()
     {
         rb.linearVelocity = new Vector2(_horizontalMovement * speed, rb.linearVelocity.y);
+        Gravity();
         animator.SetFloat("yVelocity", rb.linearVelocity.y);
         animator.SetFloat("magnitude", rb.linearVelocity.magnitude);
     }
@@ -24,5 +34,50 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         _horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+    
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (IsGrounded())
+        {
+            if (context.performed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            }
+            else if (context.canceled)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+                
+            }
+            animator.SetTrigger("jump");
+        }
+    }
+
+    private void Gravity()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
+    }
+    
+    private bool IsGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
     }
 }
